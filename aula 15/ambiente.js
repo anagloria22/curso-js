d4d5b5d3340d5f366ae0835773687e9589ba7203
Ana Gloria <147454263+anagloria22@users.noreply.github.com>
2024-03-29 16:15:21 -0300

    Update ambiente.js and create vetor.js

---
+++ aula 15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+num[3]= 6
+num.sort()
+num.push(7)
+num.length
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
---
+++ aula 15/vetor.js
@@ -0,0 +1,7 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+//console.log(valores) mostra o vetor de uma vez//
+
+for (let pos = 0; pos <valores.length; pos++) {
+    console.log(`A posição do vetor ${pos} é o valor ${valores[pos]}`)
+}
